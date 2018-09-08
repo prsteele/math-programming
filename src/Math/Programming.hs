f838fec2ea394c5198c91d1f716bf0ff58b6b017
Patrick Steele <steele.pat@gmail.com>
2018-09-08 15:42:20 -0400

    Document the methods of the LPMonad class

--- src/Math/Programming.hs
+++ src/Math/Programming.hs
@@ -22,22 +22,58 @@ import Math.Programming.Expr
 import Math.Programming.Inequality
 
 class (Monad m, Num (Numeric m)) => LPMonad m where
+
+  -- | The type of variables in the model. The LPMonad treats these as
+  -- opaque values, but instances may expose more details.
   data Variable m :: *
+
+  -- | The type of constraints in the model. The LPMonad treats these
+  -- as opaque values, but instances may expose more details.
   data Constraint m :: *
+
+  -- | The numeric type used in the model.
   type Numeric m
 
+  -- | Create a new decision variable in the model.
   addVariable :: m (Variable m)
+
+  -- | Name an existing decision variable in the model.
   nameVariable :: Variable m -> String -> m ()
+
+  -- | Delete a decision variable from the model. The variable should
+  -- no longer be used after being deleted.
   deleteVariable :: Variable m -> m ()
+
+  -- | Add a constraint to the model represented by an inequality.
   addConstraint :: Inequality (Variable m) (Numeric m) -> m (Constraint m)
+
+  -- | Name an existing constraint in the model.
   nameConstraint :: Constraint m -> String -> m ()
+
+  -- | Delete a constraint from the model. The constraint should no
+  -- longer be used after being deleted.
   deleteConstraint :: Constraint m -> m ()
+
+  -- | Set the objective function of the model.
   setObjective :: LinearExpr (Variable m) (Numeric m) -> m ()
+
+  -- | Set the optimization direction of the model.
   setSense :: Sense -> m ()
+
+  -- | Optimize the model.
   optimize :: m SolutionStatus
+
+  -- | Set the upper- or lower-bounds on a variable.
   setVariableBounds :: Variable m -> Bounds (Numeric m) -> m ()
+
+  -- | Set the domain of a variable, i.e. whether it is continuous or
+  -- integral.
   setVariableDomain :: Variable m -> Domain -> m ()
+
+  -- | Get the value of a variable in the current solution.
   evaluateVariable :: Variable m -> m (Numeric m)
+
+  -- | Get the value of a linear expression in the current solution.
   evaluateExpression :: LinearExpr (Variable m) (Numeric m) -> m (Numeric m)
 
 data Bounds b

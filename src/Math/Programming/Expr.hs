7994e40b4508653ed515b37a3eaa179595cce612
Patrick Steele <steele.pat@gmail.com>
2018-10-20 17:55:15 -0400

    Swap the ordering of type variables for linear expressions
    This will allow Functor definitions over the variables

--- src/Math/Programming/Expr.hs
+++ src/Math/Programming/Expr.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE InstanceSigs #-}
 {-| Symbolic linear expressions.
 
 This module defines operators to manipulate linear expressions. Note
@@ -11,87 +12,87 @@ module Math.Programming.Expr where
 
 import Data.List (sortOn)
 
--- | A linear expression containing variables of type @a@ and numeric
--- coefficients of type @b@.
+-- | A linear expression containing variables of type @b@ and numeric
+-- coefficients of type @a@.
 --
 -- Using strings to denote variables and 'Double's as our numeric
 -- type, we could express the term /3 x + 2 y + 1/ as
 --
 -- @
---   LinearExpr [("x", 3), ("y", 2")] 1
+--   LinearExpr [(3, "x"), (2, "y")] 1
 -- @
 data LinearExpr a b
   = LinearExpr
     { _terms :: [(a, b)]
-    , _constant :: b
+    , _constant :: a
     }
   deriving
     ( Read
     , Show
     )
 
-instance (Num b) => Semigroup (LinearExpr a b) where
+instance (Num a) => Semigroup (LinearExpr a b) where
   (<>) = (.+.)
 
-instance (Num b) => Monoid (LinearExpr a b) where
+instance (Num a) => Monoid (LinearExpr a b) where
   mempty = LinearExpr [] 0
 
-sumExpr :: (Num b) => [LinearExpr a b] -> LinearExpr a b
+sumExpr :: (Num a) => [LinearExpr a b] -> LinearExpr a b
 sumExpr = mconcat
 
-(*:) :: (Num b) => b -> a -> LinearExpr a b
-constant *: term = LinearExpr [(term, constant)] 0
+(*:) :: (Num a) => a -> b -> LinearExpr a b
+coef *: term = LinearExpr [(coef, term)] 0
 
-(.+.) :: (Num b) => LinearExpr a b -> LinearExpr a b -> LinearExpr a b
+(.+.) :: (Num a) => LinearExpr a b -> LinearExpr a b -> LinearExpr a b
 (LinearExpr terms constant) .+. (LinearExpr terms' constant')
   = LinearExpr (terms <> terms') (constant + constant')
 infixl 6 .+.
 
-(.+) :: (Num b) => LinearExpr a b -> b -> LinearExpr a b
+(.+) :: (Num a) => LinearExpr a b -> a -> LinearExpr a b
 ex .+ constant = ex .+. LinearExpr [] constant
 infixl 6 .+
 
-(+.) :: (Num b) => b -> LinearExpr a b -> LinearExpr a b
+(+.) :: (Num a) => a -> LinearExpr a b -> LinearExpr a b
 (+.) = flip (.+)
 infixl 6 +.
 
-(.-.) :: (Num b) => LinearExpr a b -> LinearExpr a b -> LinearExpr a b
+(.-.) :: (Num a) => LinearExpr a b -> LinearExpr a b -> LinearExpr a b
 ex .-. ex' = ex .+. (ex' .* (-1))
 infixl 6 .-.
 
-(.-) :: (Num b) => LinearExpr a b -> b -> LinearExpr a b
+(.-) :: (Num a) => LinearExpr a b -> a -> LinearExpr a b
 ex .- constant = ex .+ (negate constant)
 infixl 6 .-
 
-(-.) :: (Num b) => b -> LinearExpr a b -> LinearExpr a b
+(-.) :: (Num a) => a -> LinearExpr a b -> LinearExpr a b
 constant -. ex = constant +. ((-1) *. ex)
 infixl 6 -.
 
-(.*) :: (Num b) => LinearExpr a b -> b -> LinearExpr a b
+(.*) :: (Num a) => LinearExpr a b -> a -> LinearExpr a b
 LinearExpr terms constant .* coef
   = LinearExpr (fmap scale terms) (constant * coef)
   where
-    scale (x, y) = (x, y * coef)
+    scale (x, y) = (x * coef, y)
 infixl 7 .*
 
-(*.) :: (Num b) => b -> LinearExpr a b -> LinearExpr a b
+(*.) :: (Num a) => a -> LinearExpr a b -> LinearExpr a b
 (*.) = flip (.*)
 infixl 7 *.
 
-(./) :: (Num b, Fractional b) => LinearExpr a b -> b -> LinearExpr a b
+(./) :: (Num a, Fractional a) => LinearExpr a b -> a -> LinearExpr a b
 ex ./ coef = ex .* (1 / coef)
 infixl 7 ./
 
 -- | Combine equivalent terms by summing their coefficients
-simplify :: (Ord a, Num b) => LinearExpr a b -> LinearExpr a b
+simplify :: (Ord b, Num a) => LinearExpr a b -> LinearExpr a b
 simplify (LinearExpr terms constant)
-  = LinearExpr (reduce (sortOn fst terms)) constant
+  = LinearExpr (reduce (sortOn snd terms)) constant
   where
     reduce []           = []
-    reduce ((x, c): []) = [(x, c)]
-    reduce ((x, c): (x', c'): xs)
-      | x == x'   = (x, c + c') : reduce xs
-      | otherwise = (x, c) : reduce ((x', c'): xs)
+    reduce ((c, x): []) = [(c, x)]
+    reduce ((c, x): (c', x'): xs)
+      | x == x'   = (c + c', x) : reduce xs
+      | otherwise = (c, x) : reduce ((c', x'): xs)
 
 eval :: Num a => LinearExpr a a -> a
 eval (LinearExpr terms constant) = constant + sum (map (uncurry (*)) terms)

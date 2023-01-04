4e8c25f9cf8acf38bf2fd78e90fa7bdb04462cc5
Patrick Steele <steele.pat@gmail.com>
2023-01-04 00:02:30 -0500

    Include examples in default.nix

--- default.nix
+++ default.nix
@@ -4,18 +4,20 @@
 }:
 let
   overrides = self: super: {
-    glpk-headers           = self.callCabal2nix "glpk-headers"           ./glpk-headers          {};
-    math-programming       = self.callCabal2nix "math-programming"       ./math-programming      {};
-    math-programming-glpk  = self.callCabal2nix "math-programming-glpk"  ./math-programming-glpk {};
+    examples               = self.callCabal2nix "examples"               ./examples               {};
+    glpk-headers           = self.callCabal2nix "glpk-headers"           ./glpk-headers           {};
+    math-programming       = self.callCabal2nix "math-programming"       ./math-programming       {};
+    math-programming-glpk  = self.callCabal2nix "math-programming-glpk"  ./math-programming-glpk  {};
     math-programming-tests = self.callCabal2nix "math-programming-tests" ./math-programming-tests {};
   };
   hPkgs = pkgs.haskell.packages.${compiler}.override { inherit overrides; };
 in
 with hPkgs;
-  { inherit
-    glpk-headers
-    math-programming
-    math-programming-glpk
-    math-programming-tests
-    ;
-  }
+{ inherit
+  examples
+  glpk-headers
+  math-programming
+  math-programming-glpk
+  math-programming-tests
+  ;
+}
